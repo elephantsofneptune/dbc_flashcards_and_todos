bec469e0c41fec59c5f0ef3956098db678e1c34b
José Menor <josemenor@gmail.com>
2014-02-05 16:49:46 -0800

    Tried to make it using two structs, didn't work

--- part3_ruby-todos-1-core-features-challenge/source/todo.rb
+++ part3_ruby-todos-1-core-features-challenge/source/todo.rb
@@ -8,3 +8,79 @@
 
 # Note that (4) is where the essence of your application lives.
 # Pretty much every application in the universe has some version of responsibilities (1), (2), and (3).
+
+# STDIN.gets
+
+#Controller
+  # Creates lists
+  # Adds task to a list
+
+
+#View
+  # Display list
+  # Reacts to user input
+
+
+#Model
+  # Stores TODO lists
+  # Stores tasks
+
+
+#Interface
+
+Task = Struct.new(:name, :completed)
+# List = Struct.new(:name, :tasks) do
+#   :tasks = []
+#   def addTask(task)
+#     :tasks << Task.new(task, false)
+#   end
+#   def deleteTask(task)
+#     :tasks.delete(task)
+#   end
+#   def complete(task)
+#     p task#[:completed] #= true
+#   end
+# end
+
+class List
+
+  attr_reader
+
+  def initialize(name)
+    @name = name
+    @tasks=[]
+  end
+
+
+  def addTask(task)
+    @tasks << Task.new(task, false)
+  end
+
+  def deleteTask(task)
+    @tasks.delete(task)
+  end
+
+  # def complete(task)
+  #   p tasks.find(name)#[:completed]
+  # end
+
+  def tasks
+    @tasks.map { |task| task.name }
+  end
+end
+
+
+
+
+# DRIVER CODE
+# ruby todo.rb add Bake a delicious blueberry-glazed cheesecake
+# $ ruby todo.rb list
+# $ ruby todo.rb delete <task_id>
+# $ ruby todo.rb complete <task_id>
+list = List.new("People to kill")
+list.addTask("Prime Minister")
+list.addTask("Paulie Shore")
+list.addTask("Freddie Mercury")
+list.deleteTask("Freddie Mercury")
+list.complete("Prime Minister")
+puts list.tasks
