33f562b076b1f8001823052c8fef996af05f0575
menor <josemenorgmail.com>
2014-02-06 06:22:58 -0800

    Old code commented

--- part3_ruby-todos-1-core-features-challenge/source/todo.rb
+++ part3_ruby-todos-1-core-features-challenge/source/todo.rb
@@ -9,64 +9,100 @@
 # Note that (4) is where the essence of your application lives.
 # Pretty much every application in the universe has some version of responsibilities (1), (2), and (3).
 
-Task = Struct.new(:name, :completed) do
-  def complete
-    self.completed = true
-  end
-end
+# Task = Struct.new(:name, :completed) do
+#   def complete
+#     self.completed = true
+#   end
+# end
 
-class List
-  attr_reader :last_deleted
+# class List
+#   attr_reader :last_deleted
 
-  def initialize
-    @tasks=[Task.new("test1", false), Task.new("test2", false)] # test line, remove test objects!
-    @last_deleted = ''
-  end
+#   def initialize
+#     @tasks=[Task.new("test1", false), Task.new("test2", false)] # test line, remove test objects!
+#     @last_deleted = ''
+#   end
 
-  def addTask(task)
-    @tasks << Task.new(task, false)
-  end
+#   def addTask(task)
+#     @tasks << Task.new(task, false)
+#   end
 
-  def deleteTask(index)
-    @last_deleted = @tasks[index][:name]
-    @tasks.delete_at(index)
-  end
+#   def deleteTask(index)
+#     @last_deleted = @tasks[index][:name]
+#     @tasks.delete_at(index)
+#   end
 
-  def tasks
-    @tasks.map { |task| task.name }
-  end
+#   def tasks
+#     @tasks.map { |task| task.name }
+#   end
 
-end
+# end
 
 class UI
+  require 'fileutils'
 
-  def initialize(file="todo.csv")
-    @list = List.new
-    @file = File.open(file, "w")
+  def initialize(file="todo.txt")
+    # @list = List.new
+    @file = file
   end
 
-  def write_to_file
-    $stdout = @file
-  end
-
-  def parse
+  #Controller
+  def parse 
     command = ARGV[0].to_sym
     arguments = ARGV[1..-1].join(' ')
 
     case command
     when :list
-      @list.tasks.each_with_index do |task, index|
-        puts "#{index + 1}. #{task}"
-      end
+      view_list
     when :add
-      @list.addTask(arguments)
-      puts "Appended \"#{arguments}\" to your TODO list."
+      addTask(arguments)
+      taskAdded(arguments)
     when :delete
-      @list.deleteTask(arguments.to_i - 1)
-      puts "Deleted \"#{@list.last_deleted}\" from your TODO list." # Calling a method from list here = Troubles!
+      deleteTask(arguments)
+      taskDeleted(arguments)
+      #@list.deleteTask(arguments.to_i - 1)
+      # puts "Deleted \"#{@list.last_deleted}\" from your TODO list." 
     end
-
   end
+
+  def addTask(task)
+    File.open(@file, 'a') { |f| f.puts("#{task}" ) }
+  end
+
+  # def deleteTask(task)
+  #   File.open(@file, 'w+') do |f|
+  #     f.each_with_index do |line, index|
+  #       f.puts "#{index+1}. #{line}" unless index+1 == task
+  #     end
+  #   end
+  # end
+
+  def deleteTask(taskIndex)
+    FileUtils.touch("temp.txt")
+    output = File.new("temp.txt")
+    File.open(@file, 'w+').each_with_index do |task, index|
+      output.puts "#{index}. #{task}" #unless index == taskIndex
+    end
+    FileUtils.cp(@file, "#{@file}.bk")
+    FileUtils.mv("temp.txt", "todo.txt")
+  end
+
+  #VIEW
+  
+  def taskAdded(task)
+    puts "Appended \"#{task}\" to your TODO list."
+  end
+
+  def taskDeleted(task)
+    puts "Deleted \"#{task}\" from your TODO list." 
+  end
+
+  def view_list
+    File.open(@file).each_with_index do |task, index|
+      puts "#{index + 1}. #{task}"
+    end
+  end
+
 end
 
 
