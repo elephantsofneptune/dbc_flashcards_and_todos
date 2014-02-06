fc8b3cac9747aae0f7b889cc065d11c52c0b4a7d
menor <josemenorgmail.com>
2014-02-06 06:26:53 -0800

    Delete doesn't work, the rest does

--- part3_ruby-todos-1-core-features-challenge/source/todo.rb
+++ part3_ruby-todos-1-core-features-challenge/source/todo.rb
@@ -1,48 +1,7 @@
-# What classes do you need?
-
-# Remember, there are four high-level responsibilities, each of which have multiple sub-responsibilities:
-# 1. Gathering user input and taking the appropriate action (controller)
-# 2. Displaying information to the user (view)
-# 3. Reading and writing from the todo.txt file (model)
-# 4. Manipulating the in-memory objects that model a real-life TODO list (domain-specific model)
-
-# Note that (4) is where the essence of your application lives.
-# Pretty much every application in the universe has some version of responsibilities (1), (2), and (3).
-
-# Task = Struct.new(:name, :completed) do
-#   def complete
-#     self.completed = true
-#   end
-# end
-
-# class List
-#   attr_reader :last_deleted
-
-#   def initialize
-#     @tasks=[Task.new("test1", false), Task.new("test2", false)] # test line, remove test objects!
-#     @last_deleted = ''
-#   end
-
-#   def addTask(task)
-#     @tasks << Task.new(task, false)
-#   end
-
-#   def deleteTask(index)
-#     @last_deleted = @tasks[index][:name]
-#     @tasks.delete_at(index)
-#   end
-
-#   def tasks
-#     @tasks.map { |task| task.name }
-#   end
-
-# end
-
 class UI
   require 'fileutils'
 
   def initialize(file="todo.txt")
-    # @list = List.new
     @file = file
   end
 
@@ -60,8 +19,6 @@ class UI
     when :delete
       deleteTask(arguments)
       taskDeleted(arguments)
-      #@list.deleteTask(arguments.to_i - 1)
-      # puts "Deleted \"#{@list.last_deleted}\" from your TODO list." 
     end
   end
 
@@ -105,20 +62,6 @@ class UI
 
 end
 
-
-
-
 # DRIVER CODE
-# ruby todo.rb add Bake a delicious blueberry-glazed cheesecake
-# $ ruby todo.rb list
-# $ ruby todo.rb delete <task_id>
-# $ ruby todo.rb complete <task_id>
-# list = List.new("People to kill")
-# list.addTask("Prime Minister")
-# list.addTask("Paulie Shore")
-# list.addTask("Freddie Mercury")
-# list.deleteTask("Freddie Mercury")
-# list.complete("Prime Minister")
-# puts list.tasks
 list = UI.new
 list.parse
